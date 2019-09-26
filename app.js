99fe2677afb5390021f90928f9b0b60f31409919
Allan Denot <adenot@gmail.com>
2019-09-27 08:43:45 +1000

    Updating banner

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 8080
 
-app.get('/', (req, res) => res.send('Hello World: ################## DNX Solutions, a cloud-native consulting :)'))
+app.get('/', (req, res) => res.send('DNX Solutions - a cloud-native consulting'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
